39c95a7d9dc3f4d3432b97a2b60b81233f461098
Bryce Dooley <brycedooley@Bryces-MacBook-Pro.local>
2016-12-13 14:59:34 -0500

    in progress data service

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
-var links = [
-  {title: "blog", link: "/"},
-  {title: "about", link: "/about"}
-]
+var data = [];
+
+if(self.fetch){
+  fetch('../build/data.json')
+    .then(response => {
+      if(response.ok){
+      response.json()
+        .then(json => {
+          for (var i = 0; i < json.length; i++) {
+            data.push(json[i]);
+          }
+          console.log(data);
+        })
+      }else{
+        console.log('Network response was not ok.');
+      }
+    })
+    .catch(error => console.log(`Fetch error: ${error}`));
+}
 
 //TO DO
 //bring in links via json - set up json file;
@@ -10,19 +25,22 @@ var NavComponent = React.createClass({
   propTypes : {
     title : React.PropTypes.string,
     subtitle: React.PropTypes.string,
-    link : React.PropTypes.object
+    link : React.PropTypes.string
   },
-  render : function(){
+  render: function(){
+    var navLinks = data.map(item => {
+      return (
+        <li><a href={item["link"]}>{item["title"]}</a></li>
+      )
+    });
+    // console.log(navLinks);
     return (
     <nav className="row">
     <div className="box-100">
         <div className="nav_logo">bryce dooley :<span className="nav_sublogo">[web development, project management, digital marketing]</span></div>
       <div className="nav_links">
         <ul>
-          <li><a className="nav_links-active" href="#">posts</a></li>
-          <li><a href="#">about</a></li>
-          <li><a href="#">contact</a></li>
-          <li><a href="#">projects</a></li>
+          {navLinks}
         </ul>
       </div>
     </div>
